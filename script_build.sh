5b1a49c41af2ea96573d6aa1233094c3b2b9e41e
Holtz Yan <yan1166@hotmail.com>
2019-03-29 14:10:02 +0100

    algo question / correction is OK FINALLY

--- script_build.sh
+++ script_build.sh
@@ -1,6 +1,37 @@
 
 
 # -----
+# BUILD BASIC TP
+# -----
+
+# Run the basic TP .rmd document with NO correction. No modif to do.
+R
+library(rmarkdown)
+rmarkdown::render("Ggplot2_basicTP.Rmd" ) 
+q("no")
+
+# Run the basic TP .rmd document WITH correction
+cat Ggplot2_basicTP.Rmd | sed 's/class.source="Question",echo=TRUE/class.source="Question",echo=FALSE/' | sed 's/eval=FALSE,echo=FALSE/eval=TRUE,echo=TRUE/' > tmp.Rmd
+R
+library(rmarkdown)
+rmarkdown::render("tmp.Rmd" ) 
+q("no")
+mv tmp.html Ggplot2_basicTP_correction.html
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# -----
 # BUILD ADVANCED TP
 # -----
 
@@ -38,36 +69,6 @@ mv tmp Ggplot2_advancedTP.Rmd
 
 
 
-# -----
-# BUILD BASIC TP
-# -----
-
-# Put both in none
-cat Ggplot2_basicTP.Rmd | sed 's/.Question { display: block/.Question { display: none/' | sed 's/.Correction { display: block/.Correction { display: none/' > tmp
-mv tmp Ggplot2_basicTP.Rmd
-
-
-# Run the basic TP .rmd document with corrections
-cat Ggplot2_basicTP.Rmd | sed 's/.Correction { display: none/.Correction { display: block/' > tmp
-mv tmp Ggplot2_basicTP.Rmd
-R
-library(rmarkdown)
-rmarkdown::render("Ggplot2_basicTP.Rmd" ) 
-q("no")
-mv Ggplot2_basicTP.html Ggplot2_basicTP_correction.html
-
-
-# Run the basic TP .rmd document with questions
-cat Ggplot2_basicTP.Rmd | sed 's/.Correction { display: block/.Correction { display: none/' | sed 's/.Question { display: none/.Question { display: block/' > tmp
-mv tmp Ggplot2_basicTP.Rmd
-R
-library(rmarkdown)
-rmarkdown::render("Ggplot2_basicTP.Rmd" ) 
-q("no")
-
-# Put both in none
-cat Ggplot2_basicTP.Rmd | sed 's/.Question { display: block/.Question { display: none/' > tmp
-mv tmp Ggplot2_basicTP.Rmd
 
 
 
