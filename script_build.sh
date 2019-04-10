9f80400566a43b0fcde6ca63b7eaa4e6101d3198
holtzy <yan.holtz@datadoghq.com>
2019-04-10 23:20:40 +0200

    advanced ggplot2 is kind of OK

--- script_build.sh
+++ script_build.sh
@@ -26,41 +26,27 @@ rm tmp.Rmd
 
 
 
-
-
-
-
-
 # -----
 # BUILD ADVANCED TP
 # -----
 
-# Put both in none
-cat Ggplot2_advancedTP.Rmd | sed 's/.Question { display: block/.Question { display: none/' | sed 's/.Correction { display: block/.Correction { display: none/' > tmp
-mv tmp Ggplot2_advancedTP.Rmd
-
-
-# Run the advanced TP .rmd document with corrections
-cat Ggplot2_advancedTP.Rmd | sed 's/.Correction { display: none/.Correction { display: block/' > tmp
-mv tmp Ggplot2_advancedTP.Rmd
-R
-library(rmarkdown)
-rmarkdown::render("Ggplot2_advancedTP.Rmd" ) 
-q("no")
-mv Ggplot2_advancedTP.html Ggplot2_advancedTP_correction.html
-
-
-# Run the advanced TP .rmd document with questions
-cat Ggplot2_advancedTP.Rmd | sed 's/.Correction { display: block/.Correction { display: none/' | sed 's/.Question { display: none/.Question { display: block/' > tmp
-mv tmp Ggplot2_advancedTP.Rmd
+# Run the basic TP .rmd document with NO correction. No modif to do.
 R
 library(rmarkdown)
 rmarkdown::render("Ggplot2_advancedTP.Rmd" ) 
 q("no")
 
-# Put both in none
-cat Ggplot2_advancedTP.Rmd | sed 's/.Question { display: block/.Question { display: none/' > tmp
-mv tmp Ggplot2_advancedTP.Rmd
+# Run the basic TP .rmd document WITH correction
+cat Ggplot2_advancedTP.Rmd | sed 's/class.source="Question",echo=TRUE/class.source="Question",echo=FALSE/' | sed 's/,fig.show="hide",echo=FALSE//' > tmp.Rmd
+R
+library(rmarkdown)
+rmarkdown::render("tmp.Rmd" ) 
+q("no")
+mv tmp.html Ggplot2_advancedTP_correction.html
+rm tmp.Rmd
+
+
+
 
 
 
